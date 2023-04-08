154bd93d918ae8723b627e0343bb2bff54ff9a50
Victor Buendia <victorbuendia03@gmail.com>
2023-04-08 14:46:54 -0300

    Docker compose for starting Airflow

--- quickstart/start.sh
+++ quickstart/start.sh
@@ -5,10 +5,17 @@ python3 -m venv ./quickstart/venv
 # Activating virtual environment
 source ./quickstart/venv/bin/activate
 # Check if the venv is active before installing
+if [[ $1 == "-si" ]]
+then
+echo "Starting Airflow only [skipping installation]..."
+fi
 echo $(which python3)
 echo "Installing will go in 10 seconds..."
 sleep 10
 
+
+if [[ $1 != "-si" ]]
+then
 # Airflow needs a home. `~/airflow` is the default, but you can put it
 # somewhere else if you prefer (optional)
 export AIRFLOW_HOME="$(pwd)"/quickstart/airflow
@@ -32,10 +39,20 @@ pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint ./quickstart/cons
 
 # Installing pandas for examples
 pip3 install pandas
+fi
 
 # The Standalone command will initialise the database, make a user,
 # and start all components for you.
-airflow init db
+airflow db init
+
+airflow users create \
+    --username buendia \
+    --firstname Victor \
+    --lastname Buendia \
+    --role Admin \
+	--email test@gmail.com \
+	--password test
+
 airflow standalone
 
 # Visit localhost:8080 in the browser and use the admin account details
